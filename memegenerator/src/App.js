50d9bb9d1f67a9ee623e5921bc5e3daed5071d81
Ravi Teja <72485137+RaviTejaM9602@users.noreply.github.com>
2022-03-11 20:54:15 +0000

    Added Header and Meme Structure

---
+++ memegenerator/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from "./Components/Header"
+import Meme from "./Components/Meme"
+
+  export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
+
